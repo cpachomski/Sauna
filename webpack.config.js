a6e156f4e79674c1b8ec08d19f45b030b2813c15
Christopher Pachomski <christopher.pachomski@turner.com>
2017-08-21 16:41:09 -0400

    add basic config with code splitting, source maps, html plugin, and cleanup

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+module.exports = {
+    entry: {
+        index: "./src/index.js",
+        another: "./src/another-module.js"
+    },
+    // devtool: 'inline-source-map';
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    plugins: [
+        new CleanWebpackPlugin(["dist"]),
+        new HTMLWebpackPlugin({
+            title: "Universe"
+        })
+    ]
+};
