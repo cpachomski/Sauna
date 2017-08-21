c717a98c0500a2fa7c0989dd265470caf1a11ecf
Christopher Pachomski <cpachomski@gmail.com>
2017-08-21 16:43:43 -0400

    add source maps

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
         index: "./src/index.js",
         another: "./src/another-module.js"
     },
-    // devtool: 'inline-source-map';
+    devtool: "inline-source-map",
     output: {
         filename: "[name].bundle.js",
         path: path.resolve(__dirname, "dist")
