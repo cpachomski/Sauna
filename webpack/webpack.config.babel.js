ece94a2e647520bacdfa4c171757cd961d17d964
Christopher Pachomski <cpachomski@gmail.com>
2017-08-22 09:45:01 -0400

    add entry and resolve partial configs

--- webpack/webpack.config.babel.js
+++ webpack/webpack.config.babel.js
@@ -1,20 +1,18 @@
 import path from "path";
+import entry from "./entry";
 import rules from "./rules";
 import plugins from "./plugins";
-import devServer from "./dev-server";
+import resolve from "./resolve";
 
 module.exports = {
-    entry: {
-        index: path.resolve(__dirname, "../src/index.js"),
-        another: path.resolve(__dirname, "../src/another-module.js")
-    },
+    entry,
     devtool: "inline-source-map",
-    devServer,
     output: {
         filename: "[name].bundle.js",
         path: path.resolve(__dirname, "../dist")
     },
     plugins,
+    resolve,
     module: {
         rules
     }
