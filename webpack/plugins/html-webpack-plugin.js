a22438f6cecc89eb730d98c9a235d647c0984203
Christopher Pachomski <cpachomski@gmail.com>
2017-08-23 10:53:14 -0400

    update output path for dist bundle html

--- webpack/plugins/html-webpack-plugin.js
+++ webpack/plugins/html-webpack-plugin.js
@@ -5,7 +5,8 @@ const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
     title: "Universe",
     template: path.resolve(__dirname, "../../src/templates/index.ejs"),
     rootId: "universe",
-    inject: "body"
+    inject: "body",
+    filename: path.resolve(__dirname, "../../dist/index.html")
 });
 
 export default HTMLWebpackPluginConfig;
